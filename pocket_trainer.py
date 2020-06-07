d0cc12d1669a9909c512f897b223dbe755a30e44
Kurt <kpdudek2@gmail.com>
2020-06-07 00:14:24 -0400

    Workout player timer

--- pocket_trainer.py
+++ pocket_trainer.py
@@ -16,6 +16,8 @@ import os
 import json
 import pwd
 import random as rnd
+import threading
+import math
 
 class YogaMainWindow(QWidget):
     workout_creator_signal = pyqtSignal()
@@ -57,7 +59,7 @@ class YogaMainWindow(QWidget):
     def switch_to_start_session(self):
         self.workout_player_signal.emit()
 
-class WorkoutCreator(QWidget): 
+class WorkoutCreator(QWidget,FilePaths): 
     # Signals
     go_home = pyqtSignal()
 
@@ -115,8 +117,8 @@ class WorkoutCreator(QWidget):
     def load_plugins(self):
         # Get plugins from folder and load into combo box
         # Load JSON into a class attribute
-        self.user_name = pwd.getpwuid( os.getuid() ).pw_name
-        self.user_path = '/home/%s/PocketTrainer/'%self.user_name
+        # self.user_name = pwd.getpwuid( os.getuid() ).pw_name
+        # self.user_path = '/home/%s/PocketTrainer/'%self.user_name
         self.plugins = os.listdir('%sPlugins/'%(self.user_path))
         log('{} plugins found...'.format(len(self.plugins)))
 
@@ -263,7 +265,7 @@ class WorkoutCreator(QWidget):
             log("Invalid entries...")
         pass
 
-class PlaylistCreator(QWidget):
+class PlaylistCreator(QWidget,FilePaths):
     '''
     A playlist contains the plugins required, workout settings, and the workouts
     '''
@@ -293,24 +295,30 @@ class PlaylistCreator(QWidget):
 
 
 
-class WorkoutPlayer(QWidget):
+class WorkoutPlayer(QWidget,FilePaths):
     '''
     This Widget is responsible for displaying the contents of a playlist
     A plugin must be loaded whenever the workout type switches
     '''
     go_home = pyqtSignal()
 
+    pause = True
+
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Workout Player')
         self.first_launch = True
         if self.first_launch:
-            self.setGeometry(600,200,1067,600)
+            self.setGeometry(200,200,1067,600)
             self.first_launch = False
             log('Set geometry of workout player window...')
+
+        self.images_path = '%s/Images'%(self.user_path)
+        self.play_path = '%s/play.svg'%(self.images_path)
+        self.pause_path = '%s/pause.svg'%(self.images_path)
+
         self.layout = QGridLayout()
 
-        self.stop_watch = StopWatch()
         self.get_playlists()
 
         self.button = QPushButton('Close')
@@ -328,8 +336,8 @@ class WorkoutPlayer(QWidget):
         This function loads every playlist JSON file from playlists folder
         The string with no json extension is displayed in the combo box in the constructor
         '''
-        self.user_name = pwd.getpwuid( os.getuid() ).pw_name
-        self.user_path = '/home/%s/PocketTrainer/'%self.user_name
+        # self.user_name = pwd.getpwuid( os.getuid() ).pw_name
+        # self.user_path = '/home/%s/PocketTrainer/'%self.user_name
         self.playlist_files = os.listdir('%sPlaylists/'%(self.user_path))
         log('{} playlists found...'.format(len(self.playlist_files)))
 
@@ -337,7 +345,7 @@ class WorkoutPlayer(QWidget):
         if self.first_get_playlists_call:
             self.spacer = QLabel()
             self.layout.addWidget(self.spacer,0,4,10,10)
-            self.first_get_playlists_call = False
+            # self.first_get_playlists_call = False
         
         #Split the json extension of the plugin files for displaying in the QComboBox
         self.playlist_names = []
@@ -369,23 +377,96 @@ class WorkoutPlayer(QWidget):
         self.select_playlist_widget.setLayout(self.select_playlist_stack)
         self.select_playlist_stack.setAlignment(Qt.AlignCenter)
         self.layout.addWidget(self.select_playlist_widget,1,0,10,3)
-        
-    
+
     def load_playlist(self):
         '''
         The current text of the combo box gets a json extension and is then loaded from the playlists
         folder
         '''
-        if not self.first_get_playlists_call:
+        if self.first_get_playlists_call:
             try:
                 self.layout.removeWidget(self.spacer)
                 self.spacer.deleteLater()
                 self.spacer = None
+                self.first_get_playlists_call = False
             except:
                 log("Tried to remove the workout player spacer, but it doesn't exist...")
+        else:
+            try:
+                self.layout.removeWidget(self.workout_window_widget)
+                self.workout_window_widget.deleteLater()
+                self.workout_window_widget = None
+            except:
+                log("Tried to remove the workout window widget, but it doesn't exist...")
 
-        
-        pass
+        self.workout_window_widget = QWidget()
+        self.workout_window = QVBoxLayout()
+
+        self.timer_label = QLabel('00 : 00 : 00.00')
+        self.timer_label.setAlignment(Qt.AlignVCenter | Qt.AlignCenter)
+        self.workout_window.addWidget(self.timer_label)
+
+        self.timer = QTimer()
+        self.timer_count = 0.0
+        self.timer_base = 100 # in milliseconds
+        self.timer.setInterval(self.timer_base)
+        self.timer.timeout.connect(self.update_timer)
+        self.timer.start()
+
+        self.pause_play_button = QPushButton() 
+        self.pause_play_button.setIcon(QIcon(self.play_path))
+        self.pause_play_button.setIconSize(QSize(50,50))
+        # self.pause_play_button.setFixedSize(100,100)
+        self.pause_play_button.setStyleSheet("border-radius : 50; border : 0px solid black") 
+        self.pause_play_button.clicked.connect(self.pause_play)
+        self.workout_window.addWidget(self.pause_play_button)
+
+        self.workout_window_widget.setLayout(self.workout_window)
+        self.workout_window.setAlignment(Qt.AlignCenter)
+        self.layout.addWidget(self.workout_window_widget,0,4,10,10)
+
+    def pause_play(self):
+        if self.pause:
+            self.pause = False
+            self.pause_play_button.setIcon(QIcon(self.pause_path))
+        else:
+            self.pause = True
+            self.pause_play_button.setIcon(QIcon(self.play_path))
+
+    def update_timer(self):
+        if not self.pause:
+            self.timer_count += 1.0
+            self.curr_time = self.timer_count*(float(self.timer_base)/1000.0)
+            
+            hours = self.curr_time/3600.0
+            remain = hours - math.floor(hours)
+            minutes = remain * 60.0
+            remain = minutes - math.floor(minutes)
+            seconds = remain * 60.0
+
+            hours,scrap = str(hours).split('.')
+            minutes,scrap = str(minutes).split('.')
+            seconds,dec = str(seconds).split('.')
+
+            time_vals = [hours,minutes,seconds]
+            for count,val in enumerate(time_vals):
+                if len(val) < 3:
+                    try:
+                        time_vals[count] = '0'*(2-len(val)) + val
+                    except:
+                        log('Couldnt format val...')
+                        time_vals[count] = '00'
+                else:
+                    time_vals[count] = val[-2:]
+                    log('Exceeded max session time of 60 hours')
+
+            if len(dec) < 3:
+                dec = dec + '0'*(2-len(dec))
+            else:
+                dec = dec[:2]
+
+            time_label = time_vals[0] +' : '+time_vals[1]+' : '+time_vals[2]+'.'+dec
+            self.timer_label.setText(time_label)
     
     def display_workout(self,workout):
         '''
@@ -458,6 +539,12 @@ class Controller(object):
     
     def show_workout_player(self):
         self.workout_player.go_home.connect(self.show_main_window)
+        try:
+            self.main_window_past_geom = self.main_window.geometry()
+            self.main_window.hide()
+        except:
+            log("Tried to hide main window but failed...")
+
         self.workout_player.show()
 
 def main():
--- utils.py
+++ utils.py
@@ -3,6 +3,15 @@ import sys
 import time
 import datetime as dt
 import pwd
+from threading import Thread
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+class FilePaths(object):
+    user_name = pwd.getpwuid( os.getuid() ).pw_name
+    user_path = '/home/%s/PocketTrainer/'%user_name
 
 def log(text):
     '''
@@ -32,62 +41,4 @@ def log(text):
     except:
         print('Error closing log file...')
 
-class StopWatch(object):
-    # Time parameters
-    start_time = None
-    end_time = None
-    lap_times = []
-    paused_time = None
-    paused_split = None
-
-    # Flags
-    ended = False
-    paused = False
-
-    def __init__(self):
-        pass
-
-    def start(self):
-        self.start_time = dt.datetime.now()
-        pass
-    
-    def pause(self):
-        if not self.paused:
-            self.paused = True
-            self.paused_split = dt.datetime.now()
-        else:
-            log('Tried to pause timer, but it already is...')
-        pass
-
-    def play(self):
-        if self.paused:
-            self.paused = False
-            self.paused_time += dt.datetime.now()-self.paused_split
-            self.paused_split = None
-        else:
-            log('Tried to play timer, but it already is...')
-        pass
-
-    def lap(self):
-        self.lap_times.append(dt.datetime.now()-self.start_time)
-        pass
-    
-    def stop(self):
-        self.end_time = dt.datetime.now()
-        self.ended = True
-        pass
-
-    def get_time(self):
-        '''
-        Returns the time since the timer began
-        Parameters:
-            None
-        Returns:
-            hours (int)
-            minutes (int)
-            seconds (float)
-        '''
-        delt = dt.datetime.now()-self.start_time
-        hours,minutes,seconds = str(delt).split(':')
-        return int(hours),int(minutes),seconds
     
